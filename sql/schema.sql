2bf373889a03275bd7c91955d6b6e12911d8d627
EdgardD <104205251+TheEdd22@users.noreply.github.com>
2025-10-20 09:05:16 -0300

    Update schema.sql
    Script de criação das tabelas que irão armazenar os dados das Instâncias

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,10 @@
 -- Script de criação de tabelas para armazenar dados de instâncias
+CREATE TABLE instances (
+	id SERIAL PRIMARY KEY,
+	provider VARCHAR(50),
+	name VARCHAR(100),
+	location VARCHAR(100),
+	vm_size VARCHAR(50),
+	os_type VARCHAR(50),
+	ip_address VARCHAR(50)
+);
